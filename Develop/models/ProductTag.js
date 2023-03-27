ca3f29a0662ed04767221cbaf88292ef2f702eed
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-03-27 10:51:06 -0600

    added models links

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -12,29 +12,18 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: 'false'
       }
     },
-    stock: {
+    tag_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate:{
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references:{
-        model: 'category',
+      references: {
+        model: 'product',
         key: 'id',
         unique: 'false'
       }
