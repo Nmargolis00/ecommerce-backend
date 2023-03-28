0313e8d0b39c339bd7b1d81e09ce1ea45bffdd15
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-03-28 14:44:18 -0600

    got categories to work

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -52,7 +52,7 @@ router.get('/:id', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
- router.get('/:id', async (req, res) => { 
+ router.post('/:id', async (req, res) => { 
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
